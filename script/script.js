7c1cdc537a8b06a7daa51d4bd3bbfd17a90a0c78
Anar_Osmanov <anar_osmanov@vk.com>
2025-05-27 01:08:19 +0400

    Стилизация

---
+++ script/script.js
@@ -0,0 +1,6 @@
+let priceForm = document.querySelector("#price");
+let rangeInput = document.querySelector("#range-input");
+let calcPercentBtn = document.querySelector("#calc-percent");
+let resultEl = document.querySelector("#result");
+
+
