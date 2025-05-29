eada9536c2a2a0aa3106ef47932fc3fcfc203a96
Anar_Osmanov <anar_osmanov@vk.com>
2025-05-30 00:40:28 +0400

    добавление Логики

--- script/script.js
+++ script/script.js
@@ -1,6 +1,13 @@
 let priceForm = document.querySelector("#price");
 let rangeInput = document.querySelector("#range-input");
 let calcPercentBtn = document.querySelector("#calc-percent");
-let resultEl = document.querySelector("#result");
+let resultEl = document.querySelector(".result");
+let enteredPrice = document.querySelector("#entered-price");
+let currentDiscount = document.querySelector("#current-discount");
 
 
+calcPercentBtn.addEventListener("click", () => {
+    enteredPrice.textContent = `Введенная стоимость: ${priceForm.value}`;
+    currentDiscount.textContent = `Текущая скидка: ${rangeInput.value}`;
+    resultEl.textContent = `Цена с учетом скидки: ${priceForm.value - (priceForm.value * rangeInput.value) / 100}`;
+});
